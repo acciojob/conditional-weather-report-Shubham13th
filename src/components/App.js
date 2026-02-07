c6e0dbce916bc7dc0e50b5923ec2c057d0cbe183
shubh <shubhamsharmajcc@gmail.com>
2026-02-07 16:54:31 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import WeatherDisplay from "./WeatherDisplay";
 const App = () => {
+
+  const WeatherDisplayData = { temperature: 25, conditions: "Sunny" };
+
+  const [weatherData, setWeatherData] = React.useState(WeatherDisplayData);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+
+      <WeatherDisplay temperature={WeatherDisplayData.temperature} conditions={WeatherDisplayData.conditions} />
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WeatherDisplay = (props) => {
+
+    const temperatureStyle = {
+        color:props.temperature > 20 ? 'red' : 'blue'
+    }
+  return (
+    <div>
+      <h2>Weather Information</h2>
+      <p>Temperature: <span style={temperatureStyle}>{props.temperature}</span>°C</p>
+      <p>Conditions: {props.conditions}</p>
+    </div>
+  )
+}
+
+export default WeatherDisplay
